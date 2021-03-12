153be45642132b26f9abbaf421dbb7ef10e7d78b
Tuna ÖZKAN <75748254+tunazkn@users.noreply.github.com>
2021-03-12 15:29:49 +0300

    Add project files.

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class MyDictionary<TKey, TValue>
+    {
+        TKey[] _keys;
+        TValue[] _values;
+        public MyDictionary()
+        {
+            _keys = new TKey[0];
+            _values = new TValue[0];
+        }
+        public void Add(TKey key, TValue value)
+        {
+            TKey[] keys = _keys;
+            TValue[] values = _values;
+
+            _keys = new TKey[_keys.Length + 1];
+            _values = new TValue[_values.Length + 1];
+
+            for (int i = 0; i < keys.Length; i++)
+            {
+                _keys[i] = keys[i];
+                _values[i] = values[i];
+            }
+
+            _keys[_keys.Length - 1] = key;
+            _values[_values.Length - 1] = value;
+        }
+        public TKey[] Keys
+        {
+            get { return _keys; }
+        }
+
+        public TValue[] Values
+        {
+            get { return _values; }
+        }
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string, int> dictionary = new MyDictionary<string, int>();
+            dictionary.Add("Kodlama", 8);
+            dictionary.Add("Geliştirme", 2);
+            dictionary.Add("Eğitim", 5);
+            dictionary.Add("Yazılım", 10);
+            dictionary.Add("Bİlgisayar", 1);
+            dictionary.Add("C#", 9);
+            dictionary.Add("Kelime", 12);
+
+            foreach (var key in dictionary.Keys)
+            {
+                Console.WriteLine("Key: " + key);
+            }
+
+            foreach (var value in dictionary.Values)
+            {
+                Console.WriteLine("Value: " + value);
+            }
+        }
+    }
+}
